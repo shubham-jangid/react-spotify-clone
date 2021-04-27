18a0df214d88ab49a1e37c81d3673021e69a51e7
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2021-04-27 23:36:58 +0530

    top bar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,8 +4,8 @@ import "../styles/components/topBar.css";
 export default function TopBar() {
   return (
     <div className="topBar">
-      <button class="topBar__button_signin topBar__button">sign in</button>
-      <button class="topBar__buttonr_signup topBar__button">sign up</button>
+      <button class="topBar__button_signup topBar__button">sign up</button>
+      <button class="topBar__button_login topBar__button">log in</button>
     </div>
   );
 }
