90f2995af85e09c35aaa9c69dac375d543741a91
shubham-jangid <shubhamjangid70@gmail.com>
2021-05-01 11:51:17 -1200

    server modified

--- server/app.js
+++ server/app.js
@@ -4,11 +4,7 @@ const app = express();
 
 app.post("https://accounts.spotify.com/api/token", (req, res) => {
   const code = req.body.code;
-  const credentials = {
-    clientId: "334d4c8a77434a43933b31a11fcf0a56",
-    clientSecret: "c73f19f3e5634e6f81c50de98ab3cec1",
-    redirectUri: "http://localhost:3000/",
-  };
+ 
 
   const spotifyApi = new SpotifyWebApi(credentials);
 
