72531e39d582bd0c86d3ad74ca6f483a92935a1a
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2021-06-01 22:15:45 +0530

    search and album page added

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,6 +3,7 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import PlaylistBody from "./pages/PlaylistBody/PlaylistBody";
 import SearchBody from "./pages/SearchBody/SearchBody";
+import AlbumBody from "./pages/AlbumBody/AlbumBody";
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
         <Route path="/" exact component={Home} />
         <Route path="/playlist/:id" component={PlaylistBody} />
         <Route path="/search/" component={SearchBody} />
+        <Route path="/album/:id" component={AlbumBody} />
       </Switch>
     </BrowserRouter>
   );
