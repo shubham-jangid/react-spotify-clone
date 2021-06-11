f5f39f9edd3a5abd1f827e879bb7bd81b7773889
shubham-jangid/werfgth <shubhamjangid70@gmail.com>
2021-06-12 05:17:13 +0530

    some changes in redirectTUrl

--- client/src/adapters/authRequests.js
+++ client/src/adapters/authRequests.js
@@ -32,6 +32,8 @@ const redirectUrl =
     ? "http://localhost:3000/"
     : "https://spotify.shubhamjangid.in/";
 
+console.log(redirectUrl);
+
 export const AUTH_URL =
   "https://accounts.spotify.com/authorize" +
   "?response_type=code" +
